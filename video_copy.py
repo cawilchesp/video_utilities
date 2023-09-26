8fe2d2e60115312f4a8fc8bd23bf62610e40e88a
Carlos Andrés Wilches Pérez <32644658+cawilchesp@users.noreply.github.com>
2023-09-25 20:09:15 -0500

    Update video_copy.py

--- video_copy.py
+++ video_copy.py
@@ -1,13 +1,15 @@
 import supervision as sv
+import cv2
 
 from tqdm import tqdm
 import threading
+from pathlib import Path
 
 from print_info import print_video_info
 
 
 def process_video(source, video_fps) -> None:
-    target = f"{source}.mp4"
+    target = f"{Path(source).parent}/{Path(source).stem}.avi"
     
     frame_generator = sv.get_video_frames_generator(source_path=source)
     video_info = sv.VideoInfo.from_video_path(video_path=source)
@@ -15,17 +17,25 @@ def process_video(source, video_fps) -> None:
 
     print_video_info(source, video_info)
 
-    with sv.VideoSink(target_path=target, video_info=video_info) as sink:
-        for frame in tqdm(frame_generator, total=video_info.total_frames):
+    with sv.VideoSink(target_path=target, video_info=video_info, fourcc="H264") as sink:
+        for frame in tqdm(frame_generator, total=video_info.total_frames, unit='frames'):
             sink.write_frame(frame=frame)
+
+            cv2.namedWindow('output', cv2.WINDOW_KEEPRATIO)
+            cv2.imshow('output', frame)
+            cv2.resizeWindow('output', 1280, 720)
+            
+            # Stop if Esc key is pressed
+            if cv2.waitKey(1) & 0xFF == 27:
+                break
 
 
 if __name__ == "__main__":
     file_list = {
-        1: ('D:/Data/video_1.avi', 30),
-        2: ('D:/Data/video_2.avi', 30),
-        3: ('D:/Data/video_3.avi', 29.97),
-        4: ('D:/Data/video_4.mp4', 29.97)
+        1: ('D:/Data/CCT/CCT_P8_50m_60fps_030823.mp4', 59.94),
+        # 2: ('D:/Data/video_2.avi', 30),
+        # 3: ('D:/Data/video_3.avi', 29.97),
+        # 4: ('D:/Data/video_4.mp4', 29.97)
     }
 
     threads = []
